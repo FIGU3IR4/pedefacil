a2dcbfb366198d0c65a81ac80d82c5375c6d773e
Fábio Reis <fabio.greis15@gmail.com>
2025-09-27 10:53:19 -0300

    Adicionado os seguintes arquivos: PedidoController, ItemPedidoService, PedidoService ao projeto

---
+++ src/main/java/com/pedefacil/pedefacil/controller/PedidoController.java
@@ -0,0 +1,4 @@
+package com.pedefacil.pedefacil.controller;
+
+public class PedidoController {
+}
---
+++ src/main/java/com/pedefacil/pedefacil/service/PedidoService.java
@@ -0,0 +1,8 @@
+package com.pedefacil.pedefacil.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PedidoService {
+
+}
